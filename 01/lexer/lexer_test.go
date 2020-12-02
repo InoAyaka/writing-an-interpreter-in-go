7eed277b25a89809caa39a632fa3a6aa8253de5b
Ino Ayaka <60222003+InoAyaka@users.noreply.github.com>
2020-12-02 21:05:19 +0900

    [update] p.14 1.3章完了

--- 01/lexer/lexer_test.go
+++ 01/lexer/lexer_test.go
@@ -6,20 +6,61 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	input := `=+(){},;`
+	input := `
+		let five = 5;
+		let ten = 10;
+
+		let add = fn(x,y){
+			x + y;
+		};
+
+		let result = add(five,ten);
+	`
 
 	tests := []struct {
 		expextedType    token.TokenType
 		expectedLiteral string
 	}{
+		{token.LET, "let"},
+		{token.IDNET, "five"},
 		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+
+		{token.LET, "let"},
+		{token.IDNET, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+
+		{token.LET, "let"},
+		{token.IDNET, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
 		{token.LPAREN, "("},
+		{token.IDNET, "x"},
+		{token.COMMA, ","},
+		{token.IDNET, "y"},
 		{token.RPAREN, ")"},
 		{token.LBRACE, "{"},
-		{token.RBRACE, "}"},
-		{token.COMMA, ","},
+		{token.IDNET, "x"},
+		{token.PLUS, "+"},
+		{token.IDNET, "y"},
 		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+
+		{token.LET, "let"},
+		{token.IDNET, "result"},
+		{token.ASSIGN, "="},
+		{token.IDNET, "add"},
+		{token.LPAREN, "("},
+		{token.IDNET, "five"},
+		{token.COMMA, ","},
+		{token.IDNET, "ten"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+
 		{token.EOF, ""},
 	}
 
