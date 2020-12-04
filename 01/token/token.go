ae17e245101d8b3ee844b7ebb92b75d8cb3ed7a7
Ino Ayaka <60222003+InoAyaka@users.noreply.github.com>
2020-12-04 15:34:22 +0900

    [update] p.17

--- 01/token/token.go
+++ 01/token/token.go
@@ -16,8 +16,15 @@ const (
 	INT   = "INT"   //123,4,234
 
 	//演算子
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
 
 	//デリミタ
 	COMMA     = ","
