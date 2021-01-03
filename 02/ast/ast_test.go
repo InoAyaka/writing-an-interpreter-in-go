cdea7066edf7557234379f38cc44561cb5c9e7f7
Ino Ayaka <60222003+InoAyaka@users.noreply.github.com>
2021-01-03 14:48:44 +0900

    [update]p.57 テスト失敗まで

---
+++ 02/ast/ast_test.go
@@ -0,0 +1,28 @@
+package ast
+
+import (
+	"Book_waiig/02/token"
+	"testing"
+)
+
+func TestString(t *testing.T) {
+	program := &Program{
+		Statements: []Statement{
+			&LetStatement{
+				Token: token.Token{Type: token.LET, Literal: "let"},
+				Name: &Identifier{
+					Token: token.Token{Type: token.IDNET, Literal: "myVar"},
+					Value: "myVar",
+				},
+				Value: &Identifier{
+					Token: token.Token{Type: token.IDNET, Literal: "anotherVar"},
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+
+	if program.String() != "let myVar = anotherVar;" {
+		t.Errorf("program.String() wrong. got=%q", program.String())
+	}
+}
